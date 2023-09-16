b3144b4e57373d96eb1aab71511574c67c268c97
dolelongan <dolelongan@gmail.com>
2023-09-16 15:27:14 +0700

    add .mongoshrc.js

---
+++ .mongodb/.mongoshrc.js
@@ -0,0 +1,18 @@
+async function showDatabasesOnStartup() {
+    const adminDb = db.getSiblingDB('admin');
+    const { databases } = await adminDb.runCommand({ listDatabases: 1 });
+    print('Databases:');
+    for (const { name } of databases) {
+      print(`- ${name}`);
+    }
+}
+  
+showDatabasesOnStartup();
+
+let cmdCount = -1;
+prompt = function() {
+    return "\nUptime:" + db.serverStatus().uptime +
+           " Documents:" + db.stats().objects +
+           " | Cmd count: " + (cmdCount++) +
+           ` | ${db.getName()}> ` ;
+    }
