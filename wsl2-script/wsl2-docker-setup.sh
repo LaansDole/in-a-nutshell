febc91136b147668e288259b2942804a43b1ac19
Do Le Long An <85084360+LaansDole@users.noreply.github.com>
2024-04-21 15:38:20 +0700

    Create wsl2-docker-setup.sh

---
+++ wsl2-script/wsl2-docker-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Reference sites
+# https://qiita.com/gen10nal/items/1e7fe8a1b2e9ad1e7919
+# https://docs.docker.com/engine/install/ubuntu/
+# https://dev.to/0xkoji/install-docker-on-wsl2-2ma5
+
+# Install curl
+sudo apt install -y curl
+
+# Install docker via shell script
+curl -s https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-docker.sh | /bin/bash
+
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-compose-plugin
+sudo gpasswd -a $USER docker
+
+# Start docker service
+sudo service docker start
